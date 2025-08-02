a927532efe0e571043653e62a4b7408502895157
Deser7 <deser7@icloud.com>
2025-08-02 09:15:12 +0400

    создал новый проект

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Наташа Спиридонова on 02.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
