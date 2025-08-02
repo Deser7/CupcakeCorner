c987c0cd5451c679a63c40162e1cb05ef929b8a6
Deser7 <deser7@icloud.com>
2025-08-02 09:49:54 +0400

    Создал экран данных о заказе

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -8,14 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var order = Order()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section {
+                    Picker("Выберите Ваш кекс", selection: $order.type) {
+                        ForEach(Order.types.indices, id: \.self) {
+                            Text(Order.types[$0])
+                        }
+                    }
+                    
+                    Stepper("Количество кексов \(order.quantity)", value: $order.quantity)
+                }
+                
+                Section {
+                    Toggle("Добавить ингридиенты?", isOn: $order.specialRequesEnabled.animation())
+                    
+                    if order.specialRequesEnabled {
+                        Toggle("Глазурь", isOn: $order.extraFrosting)
+                        
+                        Toggle("Посыпка", isOn: $order.addSprinkles)
+                    }
+                }
+                
+                Section {
+                    NavigationLink("Детали доставки") {
+                        AdressView(order: order)
+                    }
+                }
+            }
+            .navigationTitle("Cupcake Corner")
         }
-        .padding()
     }
 }
 
--- CupcakeCorner/Order.swift
+++ CupcakeCorner/Order.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+@Observable
+final class Order {
+    static let types = ["Vanilla", "Strawberry", "Chokolate", "Rainbow"]
+    
+    var type = 0
+    var quantity = 3
+    
+    var specialRequesEnabled = false {
+        didSet {
+            if specialRequesEnabled == false {
+                extraFrosting = false
+                addSprinkles = false
+            }
+        }
+    }
+    var extraFrosting = false
+    var addSprinkles = false
+}
